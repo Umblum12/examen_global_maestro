6e0ef88fb224b0d07852ede8080a2d0679e75f81
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:43:47 -0500

    Initial commit

---
+++ examen_global_maestro/Program.cs
@@ -0,0 +1,19 @@
+﻿using examen_global_maestro;
+
+namespace examen_global_maestro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura = 90;
+            int base1 = 45;
+            int r;
+
+            r = (base1 * altura)/2;
+            Console.WriteLine(r);
+
+            Console.ReadKey(true);
+        }
+    }
+}
